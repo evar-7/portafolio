1a99ffeaa4c06f04e33f1d024b12da48de19c9b7
evar-7 <L.ev.var52@gmail.com>
2025-10-01 15:52:08 -0600

    chore: clean config.py and update requirements for Mailjet integration

--- backend/config.py
+++ backend/config.py
@@ -4,13 +4,7 @@ import os
 
 load_dotenv()
 
-MAIL_SERVER = 'smtp.office365.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
 MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-MAIL_DEFAULT_SENDER = ('Portafolio', MAIL_USERNAME)
 
 # Prints temporales para depuración en Render
 print("MAIL_USERNAME:", MAIL_USERNAME)
-print("MAIL_PASSWORD:", MAIL_PASSWORD)
